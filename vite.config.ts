632caa2d59eeaff64aab33f32a878f0afd6a517d
mairess <trabalho.maires@gmail.com>
2024-03-21 09:14:48 -0300

    feat: dockerize application

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,10 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    host: true
+  },
   test: {
     globals: true,
     environment: 'jsdom',
