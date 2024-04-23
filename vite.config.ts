e42ccccc23763afbe7f54e7211ca81807787a013
mairess <trabalho.maires@gmail.com>
2024-04-23 17:48:54 -0300

    chore: add github workflows

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@
 
 import react from '@vitejs/plugin-react-swc';
 import { defineConfig } from 'vite';
+import {codecovVitePlugin} from '@codecov/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    codecovVitePlugin({
+      enableBundleAnalysis: process.env.CODECOV_TOKEN !== undefined,
+      bundleName: "<bundle project name>",
+      uploadToken: process.env.CODECOV_TOKEN,
+    })
+  ],
   server: {
     port: 3000,
     host: true
